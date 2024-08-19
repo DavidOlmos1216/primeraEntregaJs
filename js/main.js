24fa54a2dd3f87fb3a31bf19da84a734c44d853a
davidOlmos <david.mauricio.1216@gmail.com>
2024-08-19 17:19:09 -0500

    se modifican variables

--- js/main.js
+++ js/main.js
@@ -1,38 +1,37 @@
 
-let listaDeTareas = [];
+let listaDeArticulos = [];
 
 
-function agregarTarea() {
-	let tarea = prompt("Ingrese la nueva tarea:");
-	listaDeTareas.push(tarea);
-	console.log("Tarea agregada: " + tarea);
+function agregarArticulo() {
+	let articulo = prompt("Ingrese el nombre del articulo:");
+	listaDeArticulos.push(articulo);
+	console.log("Ariticulo agregado: " + articulo);
 }
 
-function eliminarTarea() {
-	let indice = prompt("Ingrese el número de la tarea que desea eliminar:");
-	indice = parseInt(indice) - 1; // Convertir a número y ajustar el índice
-	if (indice >= 0 && indice < listaDeTareas.length) {
-		let tareaEliminada = listaDeTareas.splice(indice, 1);
-		console.log("Tarea eliminada: " + tareaEliminada);
+function eliminarArticulo() {
+	let indice = prompt("Ingrese el número del articulo que desea eliminar:");
+	indice = parseInt(indice) - 1; 
+	if (indice >= 0 && indice < listaDeArticulos.length) {
+		let articuloEliminado = listaDeArticulos.splice(indice, 1);
+		console.log("Articulo eliminado: " + articuloEliminado);
 	} else {
-		console.log("Índice inválido. La tarea no se pudo eliminar.");
+		console.log("Numero inválido. El articulo no se pudo eliminar.");
 	}
 }
 
-function mostrarTareas() {
-	console.log("Lista de tareas:");
-	for (let i = 0; i < listaDeTareas.length; i++) {
-		console.log(i + 1 + ". " + listaDeTareas[i]);
+function mostrarArticulos() {
+	console.log("Lista de Articulos:");
+	for (let i = 0; i < listaDeArticulos.length; i++) {
+		console.log(i + 1 + ". " + listaDeArticulos[i]);
 	}
 }
 
-// Ejemplo de uso de las funciones
-agregarTarea();
-agregarTarea();
-agregarTarea();
+agregarArticulo();
+agregarArticulo();
+agregarArticulo();
 
-mostrarTareas();
+mostrarArticulo();
 
-eliminarTarea();
+eliminarArticulo();
 
-mostrarTareas();
+mostrarArticulos();
